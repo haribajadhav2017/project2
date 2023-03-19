60ffc78d7416dc9aaf88b2cc19b99ddea3474b65
haribajadhav2017 <haribajadhav2017@gmail.com>
2023-03-19 15:21:21 +0530

    fistcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DealsComponent } from './deals/deals.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 const routes: Routes = [
-{path:'products/:red', component: ProductsComponent},
-{path:'product/:productId/photos',component:ProductsComponent},
-
-
-
+{path:"home",component: HomeComponent},
+  {path:"deals",component: DealsComponent},
+  {path:"fashion",component: FashionComponent},
+  {path:"electronics",component: ElectronicsComponent},
+  {path:"products/:category",component: ProductsComponent},
+  {path:"",redirectTo:"/home",pathMatch: "full"},
+  
 
 ];
 
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+selectedCategory: string | null = "";
+
+  constructor(private route: ActivatedRoute,private router:Router) {
+    this.selectedCategory = this.route.snapshot.paramMap.get("category");
+  }
+
+  ngOnInit(): void {
+
+    //filter the products depends upon the category
+
+  }
+
+  backToPrevious(){
+    this.router.navigate(['/fashion']);
+  }
 
 }
+
+
